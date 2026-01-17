3ea3d0f58daf63ef1c21a6a4128928ef4345752a
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 21:14:41 +0700

    video 20 toan tu trong javascript

---
+++ src/video20/video20.js
@@ -0,0 +1,23 @@
+
+
+console.log("video 20");
+
+// const a = 5;
+// let b = 10, c = 15;
+
+// console.log(a + b);
+
+// const d = a * b + c;
+
+// console.log(b / a);
+
+// console.log(d);
+
+// console.log(c % b);
+
+let a = 5;
+
+const c = a--; // c = a - 1;
+
+console.log(" a = " + a);
+console.log(" c = " + c);
