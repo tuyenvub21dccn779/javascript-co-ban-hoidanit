e9ecca53f089a41e22de4b1f781923421f12b5e5
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 20:19:06 +0700

    video 19 kieu du lieu tham chieu

---
+++ src/video19/video19.js
@@ -0,0 +1,19 @@
+
+
+console.log("Video 19");
+
+//object: key-value
+
+const person = {
+    name: "hoidanit",
+    age: 25,
+    address: "hanoi"
+}
+
+console.log(person);
+
+
+//array
+const scores = [10, 3, 4, 8, 9];
+
+console.log(scores);
