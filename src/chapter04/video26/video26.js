a388245dc76fc41a6e7f6befcead10b00df83d08
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 21:58:41 +0700

    video 26

---
+++ src/chapter04/video26/video26.js
@@ -0,0 +1,35 @@
+
+console.log("video 26");
+
+//if/if-else / else if
+
+const age = 12;
+
+// if (age > 18) {
+//     console.log("enjoy your video");
+// }
+
+// if (age < 18) {
+//     console.log("You can't watch this video");
+// } 
+
+if (age > 18) {
+    console.log("enjoy your video");
+} else {
+    console.log("You can't watch this video");
+}
+
+
+// score => A, B, C, D, F. Giỏi, Khá, Trung Bình
+const score = 4;
+if (score > 9) {
+    console.log("Giỏi");
+}
+
+if (score > 6 && score < 9) {
+    console.log("Khá");
+}
+
+if (score < 6) {
+    console.log("Trung Bình");
+}
