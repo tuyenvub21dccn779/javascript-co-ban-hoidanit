966b7209dd04ceedb67e8f64f65af81a26c08f46
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 21:33:07 +0700

    video 22

---
+++ src/video22/video22.js
@@ -0,0 +1,12 @@
+
+console.log("Video 22");
+
+const name = "hoidanit";
+const age = 25;
+
+const introduction = "my name is " + name + " and I'm " + age;
+console.log(introduction);
+
+const intro = `my name is "test" 'abd' ${name} 
+                and I'm ${age}`;
+console.log(intro);
