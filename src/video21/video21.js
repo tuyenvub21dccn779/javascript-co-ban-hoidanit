35d27312694c0fb5680b31d3a85997a859e2b7c4
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 21:23:56 +0700

    video 21

---
+++ src/video21/video21.js
@@ -0,0 +1,22 @@
+
+
+console.log("video 21");
+console.log("infor")
+console.error("error");
+console.warn("warn");
+
+//
+
+//
+
+/*
+afdsafsad
+a
+fdsfds
+*/
+
+const name = "eric";
+const age = 25;
+
+// Không nên: cộng chuỗi dễ sai
+console.log("Name: " + name + ", Age: " + age);
