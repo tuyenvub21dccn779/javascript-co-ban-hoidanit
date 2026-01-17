68b4bd2ee7b2d83615defbfea94ce483e9b3faf0
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-17 20:12:02 +0700

    video 18 hoidanit javascript cơ banr

---
+++ src/video18/video18.js
@@ -0,0 +1,33 @@
+
+//number 
+
+const myAge = 25;
+const myScore = 9.1;
+
+
+console.log("video 18");
+
+
+console.log(myAge);
+console.log(myScore);
+
+//string
+const name = "hoidainit";
+const name1 = 'eric';
+const name2 = `javascript`;
+
+console.log(name);
+
+
+//boolean: true/false;
+const isTuiDepTrai = true;
+
+console.log(isTuiDepTrai);
+
+//undefined
+let bla;
+console.log(bla);
+
+//null
+let testNull = null;
+console.log(testNull);
