c80fd32a3d7ccd6506a32dad46c36cafdf8ceb34
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 11:25:17 +0700

    video 30

---
+++ src/chapter04/video31/video31.js
@@ -0,0 +1,18 @@
+
+console.log("Video 31");
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+
+console.log(sum(10, 5));
+
+// (function () {
+//     console.log("say hi chay ngay di");
+// })();
