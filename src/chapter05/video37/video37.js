5d367362e83118bafaf68c359e717b5cde38666b
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 14:14:15 +0700

    video 37 array

---
+++ src/chapter05/video37/video37.js
@@ -0,0 +1,15 @@
+console.log("Video 37");
+
+
+const names = ["hoidanit", "eric", "bla bla"];
+
+const others = ["hoidanit", 25, true, [1, 2, 3]];
+
+console.log(names);
+console.log(others);
+
+
+
+
+
+
