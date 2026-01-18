6690898583f9964c1884490945e59b98d8e10778
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 11:17:23 +0700

    video 30 function

---
+++ src/chapter04/video30/video30.js
@@ -0,0 +1,18 @@
+
+
+console.log("Video 30");
+
+function greeting() {
+    console.log("say hi");
+}
+
+
+greeting();
+
+
+//sum a + b
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(6, 9));
