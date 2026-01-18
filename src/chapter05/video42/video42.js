847ef30776b668d992c3a40b54ddfd6083509f0a
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 15:05:45 +0700

    video 42

---
+++ src/chapter05/video42/video42.js
@@ -0,0 +1,28 @@
+
+console.log("Video 42");
+
+const scores = [10, 4, 8, 6];
+
+const sv1 = {
+    score: 10,
+    name: "hoidanit",
+    address: {
+        city: "hanoi",
+        country: "vietnam"
+    }
+};
+
+const sv2 = {
+    score: 4,
+    name: "eric",
+    address: {
+        city: "hanoi",
+        country: "vietnam"
+    }
+};
+
+
+const sinhvien = [sv1, sv2];
+
+console.log(">>>> scores: ", scores);
+console.log(">>>> sinhvien: ", sinhvien);
