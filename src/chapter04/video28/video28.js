7580444c1d24119e3e6b05f448fa17876527a1c8
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 11:02:59 +0700

    video 28

---
+++ src/chapter04/video28/video28.js
@@ -0,0 +1,21 @@
+
+console.log("Video 28");
+
+for (let i = 0; i <= 5; i++) {
+    console.log("i = ", i);
+}
+
+// while
+let score = 10;
+
+while (score > 50) {
+    console.log(">>> check score: ", score);
+    score--;
+}
+
+//do-while
+let score1 = 10;
+do {
+    console.log(">> check score1: ", score1);
+    score1--;
+} while (score > 50);
