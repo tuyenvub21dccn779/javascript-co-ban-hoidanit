18b00012ab5fc9e9ef9a20f3cfa34a2183ba0309
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 11:08:58 +0700

    video 29 cau lenh break va continue

---
+++ src/chapter04/video29/video29.js
@@ -0,0 +1,20 @@
+
+
+console.log("Video 29");
+
+// for (let i = 1; i < 10; i++) {
+//     console.log("i = ", i);
+//     if (i === 5) {
+//         break;
+//     }
+// }
+
+
+for (let i = 1; i < 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+
+    console.log("i = ", i);
+
+}
