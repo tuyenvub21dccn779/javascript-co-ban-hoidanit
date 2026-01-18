ec3cf85a82b090c9c5100214a88a56133b523888
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-18 14:21:48 +0700

    video 38

---
+++ src/chapter05/video38/video38.js
@@ -0,0 +1,33 @@
+
+console.log("Video 38");
+
+const names = ["hoidanit", "eric", "bla bla", "Nữ"];
+
+
+//index = 0; arr[index]
+console.log("0 = ", names[0]);
+
+console.log("10 = ", names[10]);
+
+names[2] = "update name";
+
+console.log("before ", names);
+
+names.push(true, 12);
+
+console.log("after push", names);
+
+names.unshift(null);
+
+console.log("after unshift", names);
+
+names.pop();
+console.log("after pop", names);
+
+
+
+
+
+
+
+
