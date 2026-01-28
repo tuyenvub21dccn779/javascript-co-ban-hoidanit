0b78993737c57a000776ed956d045e697d7e55d5
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-29 00:53:08 +0700

    chapter 06: video 51 Lắng nghe event với addEventListener

---
+++ src/chapter06/video51/video51.js
@@ -0,0 +1,10 @@
+console.log("video 51");
+
+const element = document.getElementById("hoidanitBtn");
+
+const handleClickBtn = () => {
+  console.log("you click a button");
+};
+
+element.addEventListener("click", handleClickBtn);
+console.log(element);
