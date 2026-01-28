3b047c18e02813c8ee40bfe3b23bea4345767fa6
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-29 01:54:54 +0700

    chapter 06: video 54 Alert (extra)

---
+++ src/chapter06/video54/video54.js
@@ -0,0 +1,10 @@
+console.log("video 54");
+
+const element = document.getElementById("login");
+
+element.addEventListener("click", () => {
+  console.log("click me");
+  alert("your message");
+});
+
+console.log(element);
