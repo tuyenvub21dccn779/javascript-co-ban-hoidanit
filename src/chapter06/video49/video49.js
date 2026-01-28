10851e5bcf7df56cac484bc7270082b9b3e47ee2
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-28 21:42:14 +0700

    chapter06 video 49 DOM là gì

---
+++ src/chapter06/video49/video49.js
@@ -0,0 +1,9 @@
+console.log("video 49");
+
+const element = document.getElementById("username");
+
+const myDiv = document.querySelector(".hoidanit");
+
+const allMyDiv = document.querySelectorAll(".hoidanit");
+
+console.log(allMyDiv);
