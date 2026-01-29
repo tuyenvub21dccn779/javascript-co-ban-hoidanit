beaf2e777b29ff4b09ffa1db03a97dd56a3dc54a
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-29 17:33:16 +0700

    chapter 07 video 67 Chữa Bài Tập Lab 05

--- src/chapter07/video67_lab05/video67_lab05.js
+++ src/chapter07/video67_lab05/video67_lab05.js
@@ -5,20 +5,18 @@ const fetchData = async () => {
   const data = await res.json();
 
   console.log(data);
-  const tbodyElement = document.getElementById("users").querySelector("tbody");
-  const innerBodyTableString = data
-    .map((item) => {
-      return `<tr>
-        <td>${item.id}</td>
-        <td>${item.name}</td>
-        <td>${item.email}</td>
-    </tr>`;
-    })
-    .join("\n");
-  console.log(innerBodyTableString);
-
-  console.log(tbodyElement);
-  tbodyElement.innerHTML = innerBodyTableString;
+  const tbodyElement = document.querySelector("#users tbody");
+  if (data && data.length) {
+    data.forEach((user, index) => {
+      tbodyElement.innerHTML += `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+        </tr>
+        `;
+    });
+  }
 };
 
 fetchData();
